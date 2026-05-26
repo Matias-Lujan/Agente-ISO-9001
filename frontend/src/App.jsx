d8778e76b57c670f11162668b3fc10930cbdaa2e
TomasDerrossi <tomyderrossi1@gmail.com>
2026-05-26 19:43:11 -0300

    feat: inicializacion de React con Vite, pantalla de auditoria

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AuditoriaExecutionScreen from './AuditoriaExecutionScreen'; 
+
+function App() {
+  return (
+    <div>
+      <AuditoriaExecutionScreen />
+    </div>
+  );
+}
+
+export default App;
